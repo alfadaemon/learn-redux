9d12e30424346de818b7555b415e557470850bd2
alfadaemon <lf.araya@gmail.com>
2018-11-20 09:56:19 -0600

    Adds grid and single component.

---
+++ client/components/PhotGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+    render(){
+        return(
+            <div className="single-grid">
+                I'm the single view.
+            </div>
+        )
+    }
+});
+
+export default Single;
